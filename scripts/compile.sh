38dfaa7d272ed848fe8ab56cec18478c5d214dd8
Gabriel Soares <gabrielsoaresm94@gmail.com>
2021-11-07 01:20:03 -0300

    finaliza novo ci

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,7 +5,7 @@ mapfile -t codesArr < codes.md
 while IFS= read -r line
 do
     VARIABLE=$(echo $line | grep -P '[^\/]+(?=\.)' -o)
-    echo "$VARIABLE=$VARIABLE"
+    # echo "$VARIABLE=$VARIABLE"
     gcc ./code/$VARIABLE.c -o ./$VARIABLE.out
-    ./$VARIABLE.out
+    # ./$VARIABLE.out
 done < codes.md
