104c0372910278e57f9b0ff040492c39c8ec94b4
Gabriel Soares <gabrielsoaresm94@gmail.com>
2021-11-06 23:55:16 -0300

    testando 7x

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,4 +1,12 @@
-# git log -p -1 .github/workflows/ci.yml
-# for (file in files) {
-#     gcc file    
-# }
+#!/bin/bash
+
+VARIABLE=""
+ARRAY=()
+mapfile -t codesArr < codes.md
+while IFS= read -r line
+do
+    echo $line | grep -P '[^\/]+(?=\.)' -o
+    VARIABLE=$(echo $line | grep -P '[^\/]+(?=\.)' -o)
+    ARRAY+=($VARIABLE)
+done < codes.md
+echo ${ARRAY[*]}
