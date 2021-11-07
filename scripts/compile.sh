247a9cafe007baa61513221a777b96286ca58aae
Gabriel Soares <gabrielsoaresm94@gmail.com>
2021-11-07 00:16:31 -0300

    testando 8x

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 VARIABLE=""
-ARRAY=()
+# ARRAY=()
 mapfile -t codesArr < codes.md
 while IFS= read -r line
 do
     VARIABLE=$(echo $line | grep -P '[^\/]+(?=\.)' -o)
-    ARRAY+=($VARIABLE)
+    # ARRAY+=($VARIABLE)
+    echo "::set-env name=$VARIABLE::$VARIABLE"
 done < codes.md
-echo ${ARRAY[*]}
+# echo ${ARRAY[*]}
