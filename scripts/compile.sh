f17894dc43ae5c6fffa95441d6ab243e19221df4
Gabriel Soares <gabrielsoaresm94@gmail.com>
2021-11-06 23:57:42 -0300

    testando 7x

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,7 +5,6 @@ ARRAY=()
 mapfile -t codesArr < codes.md
 while IFS= read -r line
 do
-    echo $line | grep -P '[^\/]+(?=\.)' -o
     VARIABLE=$(echo $line | grep -P '[^\/]+(?=\.)' -o)
     ARRAY+=($VARIABLE)
 done < codes.md
